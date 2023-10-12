d88010c207eba9d5a5f84f30055c35b9beade60c
Evaldo Cardoso <evaldorcardoso@outlook.com>
2023-10-12 11:57:05 -0300

    feat: home navbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,13 @@ import { useProfile } from '@/support/spotifyApi';
 import { onMounted } from 'vue';
 import { useUserStore } from '@/store/userStore'
 import router from '@/router';
+import { ref } from 'vue';
+import { useDisplay } from 'vuetify';
 
   const { getProfile } = useProfile();
   const userStore = useUserStore()
+  const drawer = ref(false)
+  const { mdAndUp } = useDisplay()
 
   const getUserProfile = async() => {
     if (!userStore.getUser) {
@@ -20,8 +24,8 @@ import router from '@/router';
 </script>
 
 <template>
-  <v-layout class="rounded rounded-md">
-    <v-navigation-drawer expand-on-hover rail class="navbar">
+   <!-- <v-layout class="rounded rounded-md">
+   <v-navigation-drawer absolute left class="navbar" v-model="drawer" :permanent="mdAndUp">
       <v-list>
         <v-list-item
           :prepend-avatar="userStore.getUser?.images[0].url"
@@ -36,7 +40,14 @@ import router from '@/router';
         <v-list-item prepend-icon="fas fa-sign-out-alt" title="Logout" value="logout"></v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar class="navbar" title="Playlist Organizer"></v-app-bar>
+    <v-toolbar color="white" absolute right>
+      <v-toolbar-side-icon
+          class="hidden-md-and-up"
+          @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-app-bar class="navbar" title="Playlist Organizer">      
+    </v-app-bar>
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
       Main Content
     </v-main>
@@ -45,7 +56,27 @@ import router from '@/router';
         made by Evaldo R.C.
       </v-btn>
     </v-footer>
-  </v-layout>
+  </v-layout> -->
+
+  <v-app>
+    <v-app-bar color="#212121" class="flex-grow-0" app dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Playlist Organizer</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer color="grey-darken-4" app v-model="drawer" expand-on-hover>
+      <v-list-item
+        :prepend-avatar="userStore.getUser?.images[0].url"
+        :title="userStore.getUser?.display_name"
+        :subtitle="userStore.getUser?.email"
+      ></v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item prepend-icon="fas fa-home" title="Dashboard" value="dashboard" @click="router.push('/')" />
+        <v-list-item prepend-icon="fas fa-book" title="My Library" value="library"></v-list-item>
+        <v-list-item prepend-icon="fas fa-sign-out-alt" title="Logout" value="logout"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
 </template>
 
 <style scoped>
